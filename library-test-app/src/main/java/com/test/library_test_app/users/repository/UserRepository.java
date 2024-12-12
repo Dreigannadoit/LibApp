e97309a4b00353440db2e25ebe9c23b4131dea9a
John Kerby Cerda <jkcerda145@gmail.com>
2024-12-13 02:25:07 +0800

    Add files via upload

--- library-test-app/src/main/java/com/test/library_test_app/users/repository/UserRepository.java
+++ library-test-app/src/main/java/com/test/library_test_app/users/repository/UserRepository.java
@@ -2,8 +2,9 @@ package com.test.library_test_app.users.repository;
 
 import com.test.library_test_app.users.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserName(String userName); // Query for authentication
 }
